89b3807641edf5d7d7f2fd9ceff25f9825e533f1
suban <nursuluu@gmx.de>
2022-07-19 16:00:24 +0200

    commit

--- src/animal/Main.java
+++ src/animal/Main.java
@@ -3,26 +3,40 @@ package animal;
 public class Main {
     public static void main(String[] args){
 
+
         Animals[] animals = {new Shark(),
                              new Turtle(),
-                              new Eagle()};
-        for (Animals a : animals) {
+                             new Eagle()};
+        for (Animals a : animals){
+
             if (a instanceof Shark) {
-                a.view();
+                ((Shark) a).attack();
 
-                for (Animals a2 : animals) {
-                    if (a2 instanceof Eagle) {
-                        a2.view();
 
-                        for (Animals a3 : animals) {
-                            if (a3 instanceof Turtle) {
-                                a3.view();
+            }else if (a instanceof Turtle) {
+                ((Turtle) a).swim();
 
-                                System.out.println("----------------");
+            } else if (a instanceof Eagle){
+                ((Eagle) a).fly();
 
-                                for (Animals a1 : animals) {
+                System.out.println("----------------");
+
+                for (Animals a1 : animals) {
                     System.out.println(a1.getClass().getName());
                 }
 
+
             }
-        }}}}}}}
+        }
+        }
+        }
+
+
+
+
+
+
+
+
+
+
